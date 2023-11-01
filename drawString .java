b0d29c036e63221406eaaf9a24acda1e296b22be
pramodsaini <pramodsaini25062000@gmail.com>
2023-10-31 20:41:33 -0700

    this is draw the string using the applet graphics at position of the x is 100 and y is 100

---
+++ drawString .java
@@ -0,0 +1,9 @@
+package Applet_java;
+import java.awt.*;
+import java.applet.*;
+public class drawString  extends Applet {
+    public void paint(Graphics g){
+        g.drawString("Welcome",100,100);
+    }
+}
+//<applet code ="drawString.java" width=500 heigth=500></applet>
