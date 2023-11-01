560f8179e55ef2d20a118a6917b369550eb9977b
pramodsaini <pramodsaini25062000@gmail.com>
2023-10-31 20:47:44 -0700

    this is draw the rectangle using the applet which have the x direction is 100 and y cordinate is 100 and width is 100 and also heigth is 100

---
+++ drawRect.java
@@ -0,0 +1,9 @@
+package Applet_java;
+import java.awt.*;
+import java.applet.*;
+public class drawRect {
+    public void paint(Graphics g){
+        g.drawRect(100,100,100,100);
+    }
+}
+//<applet code="drawRect.java" width=500 height=500></applet>
