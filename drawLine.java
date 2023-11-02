5d93b14cd7999bfe56c29230e9541777d15af391
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-01 21:03:50 -0700

    this is draw the line in the applet which have the x1 position is 50 and  y1 position is 100 and x2 position is 70 and y2 position is 170

---
+++ drawLine.java
@@ -0,0 +1,9 @@
+package Applet_java;
+import java.awt.*;
+import java.applet.*;
+public class drawLine {
+    public void paint(Graphics g){
+        g.drawLine(50, 100, 70, 170);
+    }
+}
+//<applet code ="drawLine.java" width=500 height=500></applet>
