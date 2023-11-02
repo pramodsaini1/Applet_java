75cde694f374051b3d9db28d11ec81247095d98a
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-01 20:48:12 -0700

    this is draw the oval in applet which have the x position is 100 and y position is 100 and width is 100 and also height is 100

---
+++ drawOval.java
@@ -0,0 +1,9 @@
+package Applet_java;
+import java.awt.*;
+import java.applet.*;
+public class drawOval {
+    public void paint(Graphics g){
+        g.drawOval(100,100,100,100);
+    }
+}
+//<applet code ="drawOval.java" width=500 height=500></applet>
