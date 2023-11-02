58608141ee6596a4a7e1622d37a9efb739af30b0
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-01 20:54:23 -0700

    this is draw the arc in applet which is have the x position is 100 and y position is 100 and width is 100 and also height is 100 and starting angle is 90 and make the arc is 30

---
+++ drawArc.java
@@ -0,0 +1,9 @@
+package Applet_java;
+import java.awt.*;
+import java.applet.*;
+public class drawArc {
+    public void paint(Graphics g){
+        g.drawArc(100, 100, 100, 100, 90, 30);
+    }
+}
+//<applet code ="drawArc.java" width=500 height=500></applet>
