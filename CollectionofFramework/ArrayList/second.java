60afe948240e26aa68626691e478ffa790e78e2b
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-06 20:13:10 -0800

    this is add the element in the arraylist in the collection of the framework

---
+++ CollectionofFramework/ArrayList/second.java
@@ -0,0 +1,12 @@
+package Applet_java.CollectionofFramework.ArrayList;
+import java.util.*;
+public class second {
+    public static void main(String []arr){
+        ArrayList ls = new ArrayList<>();
+        for(int i=1;i<=10;i++){
+            ls.add(i);
+        }
+        ls.add(11);
+        System.out.println(ls);
+    }
+}
