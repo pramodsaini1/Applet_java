65cd804f1802554eaa042ddbe0bc194e22b1ced8
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-06 20:01:17 -0800

    this is basic example of the arraylist in the collection of the framework

---
+++ CollectionofFramework/ArrayList/one.java
@@ -0,0 +1,12 @@
+package Applet_java.CollectionofFramework.ArrayList;
+import java.util.*;
+public class one {
+    public static void main(String []arr){
+        ArrayList ls = new ArrayList<>();
+        for(int i=1;i<=10;i++){
+            ls.add(i);
+        }
+        System.out.println(ls);
+    }
+}
+
