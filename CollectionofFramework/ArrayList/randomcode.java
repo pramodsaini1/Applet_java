30df10ebe7b399b851c2380740ea003029bfb5be
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-07 19:43:34 -0800

    this is arraylist is created the random code

--- CollectionofFramework/ArrayList/randomcode.java
+++ CollectionofFramework/ArrayList/randomcode.java
@@ -1,6 +1,26 @@
 package Applet_java.CollectionofFramework.ArrayList;
 import java.util.*;
 public class randomcode {
-    ArrayList ls=new ArrayList<>();
+       
+      public static void main(String []arr){
+             ArrayList ls=new ArrayList<>();
+            String code="";
+            for(char c='A';c<='Z';c++){
+                ls.add(String.valueOf(c));
+            }
+            for(char c='a';c<='z';c++){
+                ls.add(String.valueOf(c));
+            }
+            for(int i=1;i<=9;i++){
+                ls.add(i);
+            }
+            Collections.shuffle(ls);
+            for(int i=0;i<6;i++){
+                code = code+ls.get(i);
+            }
+            System.out.println(code);
+      }
+
+
     
 }
