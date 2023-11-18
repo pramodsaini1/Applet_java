bfd3a88d83d65aa1dc492450a72f6236bda3053b
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-17 19:44:55 -0800

    this is all about the linking of the frame

--- jframe/Edit.java
+++ jframe/Edit.java
@@ -1,5 +1,43 @@
 package Applet_java.jframe;
+import java.awt.*;
+import javax.swing.*;
+import java.applet.*;
+import java.awt.event.*;
+public class Edit extends JFrame implements ActionListener {
+    JButton jb1,jb2,jb3;
 
-public class Edit {
-    
+      public Edit (String s){
+        super(s);
+        setLayout(new FlowLayout());
+
+        jb1=new JButton("Home");
+        jb2=new JButton("Insert");
+        jb3=new JButton("Delete");
+
+        jb1.addActionListener(this);
+        jb2.addActionListener(this);
+        jb3.addActionListener(this);
+
+      }
+      public void actionPerformed(ActionEvent e){
+           if(e.getSource()==jb1){
+              Home d=new Home("Home Frame");
+              d.setSize(500,500);
+              d.setVisible(true);
+              this.setVisible(false);
+           }
+           else if(e.getSource()==jb2){
+               Inserta d=new Inserta("Insert Frame");
+              d.setSize(500,500);
+              d.setVisible(true);
+              this.setVisible(false);
+           }
+           else{
+                Delete d=new Delete("Delete Frame");
+              d.setSize(500,500);
+              d.setVisible(true);
+              this.setVisible(false);
+           }
+
+      }
 }
--- jframe/inserta.java
+++ jframe/inserta.java
@@ -1,5 +1,44 @@
 package Applet_java.jframe;
+import java.awt.*;
+import javax.swing.*;
+import java.applet.*;
+import java.awt.event.*;
+public class inserta extends JFrame implements ActionListener {
+    JButton jb1,jb2,jb3;
 
-public class inserta {
-    
+      public inserta(String s){
+        super(s);
+        setLayout(new FlowLayout());
+
+        jb1=new JButton("Home");
+        jb2=new JButton("Edit");
+        jb3=new JButton("Delete");
+
+        jb1.addActionListener(this);
+        jb2.addActionListener(this);
+        jb3.addActionListener(this);
+
+      }
+      public void actionPerformed(ActionEvent e){
+           if(e.getSource()==jb1){
+              Home d=new Home("Insert Frame");
+              d.setSize(500,500);
+              d.setVisible(true);
+              this.setVisible(false);
+           }
+           else if(e.getSource()==jb2){
+               Edit d=new Edit("Edit Frame");
+              d.setSize(500,500);
+              d.setVisible(true);
+              this.setVisible(false);
+           }
+           else{
+                Delete d=new Delete("Delete Frame");
+              d.setSize(500,500);
+              d.setVisible(true);
+              this.setVisible(false);
+           }
+
+      }
+      
 }
